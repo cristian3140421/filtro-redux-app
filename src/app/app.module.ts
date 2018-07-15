0eb374073a98fa389b3549857f477cb177a23c7e
Cristian Marin <marinhoyoscristianandres@gmail.com>
2018-07-15 17:37:57 -0500

    Aplicacion de produccion lista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
 import { TodoComponent } from './todo/todo.component';
 import { TodoListComponent } from './todo/todo-list/todo-list.component';
 import { TodoItemComponent } from './todo/todo-item/todo-item.component';
 import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
 import { TodoAddComponent } from './todo/todo-add/todo-add.component';
+
+import { FooterComponent } from './footer/footer.component';
 
 import {ReactiveFormsModule} from '@angular/forms';
 
@@ -21,13 +22,13 @@ import { FilterPipe } from './filter/filter.pipe';
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent,
     TodoComponent,
     TodoListComponent,
     TodoItemComponent,
     TodoFooterComponent,
     TodoAddComponent,
-    FilterPipe
+    FilterPipe,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
